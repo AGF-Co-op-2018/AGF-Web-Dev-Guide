0946ca310278bfaab407ccbd076b28056827e683
Peter Nguyen <peternguyenftw@hotmail.com>
2018-07-19 13:32:48 -0400

    Added iReport section, adjusted layout, added Modals, fixed back to top button

--- top.js
+++ top.js
@@ -1,9 +1,15 @@
+var status = "false";
 $(window).scroll(function() {
     var height = $(window).scrollTop();
-    if (height > 100) {
-        $('#topBtn').fadeIn();
-    } else {
-        $('#topBtn').fadeOut();
+    if (height > 100 && status == "false") {
+        $('#topBtn').css('display', 'block');
+        $('#topBtn').fadeTo(400, 1);
+        status = "true";
+    } else if(height <= 100 && status == "true") {
+        $('#topBtn').fadeTo(400, 0, function() {
+            $('#topBtn').css('display', 'none');
+        });
+        status = "false";
     }
 });
 $(document).ready(function() {
@@ -12,4 +18,8 @@ $(document).ready(function() {
         $("html, body").animate({ scrollTop: 0 }, "slow");
         return false;
     });
+     $('.col-6 img').on('click', function() {
+            $('.modal-image').attr('src', $(this).attr('src'));
+            $('#imgModal').modal('show');
+        });
 });
